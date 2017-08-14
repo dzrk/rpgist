5e6e6db901dc9a4a123965aadeffb09d4f4ae3ad
Derrick <s3546900@student.rmit.edu.au>
2017-08-15 09:26:23 +1000

    Initial Commit

---
+++ RPG/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RPG
+//
+//  Created by Derrick Phung on 15/8/17.
+//  Copyright © 2017 Derrick. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
