23329d3b3af5417d1e8866019334ee9b7161756c
Priscilla Jofani Oetomo <s3669534@w8408186.cs.rmit.edu.au>
2017-09-14 17:06:03 +1000

    Initial Commit

---
+++ RPGistFrontEnd/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RPGistFrontEnd
+//
+//  Created by Priscilla Jofani Oetomo on 9/14/17.
+//  Copyright © 2017 Priscilla Jofani Oetomo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
